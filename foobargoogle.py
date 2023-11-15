d0c2c890e5ee11c804bb554d15e1c85d673bd2a0
chobotis <rkimcho343@gmail.com>
2023-11-15 15:39:33 -0800

    added foobargoogle.py, job_desc.txt, edited parser section

---
+++ foobargoogle.py
@@ -0,0 +1,27 @@
+##LEVEL 1##
+# Write a function solution(area)
+# that takes as its input a single unit of measure
+# representing the total area of solar panels you have
+# (between 1 and 1000000 inclusive) and
+# returns a list of the areas of the largest squares
+# you could make out of those panels, starting
+# with the largest squares first.
+# So, following the example above, solution(12) would return [9, 1, 1, 1].
+ #list = []
+ #   while zooble != 0:
+ #       largest_area = int(zooble ** 0.5) ** 2
+
+  #      list.append(largest_area)
+
+   #     zooble -= largest_area
+
+ #   return list
+
+
+# Example usage:
+#zooble = 15324
+#list = foobar(zooble)
+#print(list)
+
+
+def foobar(zooble):
